1a4043fe0e180b62a8101409e54c29b028c70718
A06 <160205029+Alishba06@users.noreply.github.com>
2024-02-21 15:00:43 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,117 @@
+// Qestion 2
+// console.log("Hello world")
+
+//Question 3
+// let personName: string = 'javed Iqbal';
+// console.log(personName.toLowerCase());
+// console.log(personName.toUpperCase());
+// console.log(personName.replace(/\j\I/g, (abcd) => abcd.toUpperCase()));
+
+
+
+//Question 4
+// console.log("Life is like riding a bicycle.\" To maintain your balance, you must keep moving\"")
+
+
+
+//Question 5
+// let famous: string="Albert Einstein"
+
+// let message ="Life is like riding a bicycle. To keep your balance, you must keep moving."
+ 
+// console.log(famous, message);
+
+//Question 6
+// let  space = "\n\t Alishba \n\t";
+//  console.log(space);
+// let withoutspace = space.trim();
+
+// console.log(withoutspace);
+
+
+//Question 7
+// let additionResult: number = 7 + 1;
+// console.log  (7 + 1);
+
+// let subtractionResult: number = 11 - 3;
+
+// console.log( 11 - 3 );
+
+
+// let multiplicationResult: number = 2 * 4;
+// console.log( 2 * 4);
+
+
+// let divisionResult: number = 64 / 8;
+// console.log( 64 / 8);
+
+
+//Question 8
+// console.log( 7 + 1);
+
+// console.log( 11 - 3);
+
+// console.log( 2 * 4)
+
+// console.log( 64 / 8)
+
+
+//Question 9
+// let favoriteNumber: Number = 2 
+
+// let Message: string = "My Favorit  Number is 2"
+
+// console.log(favoriteNumber , Message)
+
+
+
+
+
+
+//Question10
+//My name is Alishba
+//Date: Ferbruary  20 , 2024
+
+// let famousName: string = "Sir Francis Bacon"
+
+// let Quote : string = "Knowledge is power"
+
+// console.log(Quote + famousName)
+
+
+//Name Alishba
+//Date: Frbruary
+//Numer 8
+// console.log(7 + 1)
+
+// console.log(11 -3)
+
+// console.log(2 * 4)
+
+// console.log(64 / 8)
+
+
+//Question 11
+// let friensdName = ["Noor Fatima", "Fiza", "Areeba"];
+// friensdName.forEach(friensdName => console.log(friensdName));
+
+
+//Question 12
+// let Names: string[] = ["Noor Fatima", "Fiza", "Areeba" ]
+// let message: string = "Hello, my best friends name  "
+// console.log(Names[0] + " "+ message);
+// console.log(Names[1] + " "+ message);
+// console.log(Names[2] + " "+ message);
+// console.log(Names[3] + " "+ message);
+
+
+//Question 13
+let  transportation: string[] = ["Audi", "luxury",]
+let message: string[] = [ "it is my  favorit car Audi and luxury"]
+console.log(transportation[0] +" "+ message)
+console.log(transportation[1] +" "+ message)
+
+
+
+
+//Quiestion 14
